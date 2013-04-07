197e231231a8de1a63d807542fd44610752ecce1
GeissT <geisst-au@hotmail.com>
2013-04-07 22:53:29 +0930

    new method started

--- mailbox.rb
+++ mailbox.rb
@@ -1,24 +1,45 @@
 class Mailbox
+    VERSION = "0.1d"
     def initialize(*args)
         @args = args
         @user = ENV['USER']
 
         # Create the .mailbox directory in the users home directory if it doesnt exist 
         Dir.mkdir(ENV['HOME'] + '/.mailbox') unless File.exists?(ENV['HOME'] + '/.mailbox')
-
+        
         if @args[0] == '-n'
             new
-        elsif @args[0] == '-f'
-            fetch
         elsif @args[0] == '-d'
-            delete 
-        else
+            delete
+        elsif @args[0] == '-h'
             help
+        elsif @args[0] == '-v'
+            puts "Version #{VERSION}"
+            puts "2013 GNU GPL. Author: GeissT"
+        else
+            fetch
         end
     end
 
     def new
+        print "Recipient: "
+        recip = STDIN.gets.chomp!
+        print "Message: "
+        body = STDIN.gets.chomp!
 
+        mailfile = "/home/" + recip + "/.mailbox/new.mail"
+
+        # Read contents so we can append
+        contents = File.read(mailfile)
+
+        body << " - " + @user
+
+        File.open("/home/#{recip}/.mailbox/new.mail", 'w') do |f|
+            message = contents + body + "\n"
+            f.write(message)
+        end
+
+        puts "Mail sent!\n"
     end
 
     def fetch 
